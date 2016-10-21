b001548b63857990d14ffc9b6420e366e5041c0f
Gavindeed <491887485@qq.com>
2016-10-21 11:21:33 +0800

    first commit

---
+++ params.h
@@ -0,0 +1,5 @@
+#define COUNTER_SIZE 8
+#define WORD_SIZE 64
+#define COUNTER_WORD 8
+
+typedef long long lint;
---
+++ sketch.h
@@ -0,0 +1,20 @@
+#include "params.h"
+
+struct Counter
+{
+	lint counter : COUNTER_SIZE;
+};
+
+class Sketch
+{
+private:
+	int width;
+	int depth;
+	Counter **sketch;
+public:
+	Sketch(int d, int w);
+	~Sketch();
+	virtual void Insert(char *str);
+	virtual lint Query(char *str);
+	virtual void Delete(char *str);
+};
